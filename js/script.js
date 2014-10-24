400016f57658d34f074b49a1526af6e4ebf68580
Rich Ramsland <rramsland@gmail.com>
2014-10-24 04:58:04 -0400

    Added JS to fix hidden nav problem
    If you were to hide the nav with the hamburger button then resize the
    window past the 665px mark the media query would not work and the nav
    would still be hidden. Fixed the issue with a little jquery magic.
    
    Also changed the media query trigger mark from 660 to 665 to provide a
    smoother transition

--- js/script.js
+++ js/script.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
     $(".hamburger").click(function(){
         $("#tooplate-menu").slideToggle(400);
     });
+
+    $(window).resize(function() {
+        if( $(window).width() > 645 ) {
+        $('#tooplate-menu').show();
+    }
+    });
 });
